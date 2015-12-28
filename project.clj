c3a1c5b84311c7d7f9542a32f2c06009828d7233
Markus Hjort <markus.hjort@gmail.com>
2015-12-28 13:58:11 +0200

    Release version 0.0.1

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject clj-kinesis-client "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-kinesis-client "0.0.1"
+  :description "Clojure AWS Kinesis client"
+  :url "https://github.com/adtile/clj-kinesis-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.amazonaws/amazon-kinesis-client "1.6.1" :exclusions [com.amazonaws/aws-java-sdk-dynamodb]]])
+                 [com.amazonaws/amazon-kinesis-client "1.6.1" :exclusions [com.amazonaws/aws-java-sdk-dynamodb]]]
+  :signing {:gpg-key "webmaster@adtile.me"})
