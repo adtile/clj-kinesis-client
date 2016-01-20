dcfb18407d698d8925210c7935484be1edfd6f54
Markus Hjort <markus.hjort@gmail.com>
2016-01-20 15:53:55 +0200

    Release version 0.0.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-kinesis-client "0.0.4"
+(defproject clj-kinesis-client "0.0.5"
   :description "Clojure AWS Kinesis client"
   :url "https://github.com/adtile/clj-kinesis-client"
   :license {:name "Eclipse Public License"
