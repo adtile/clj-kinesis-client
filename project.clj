d80a2a93de2c1be85420c43f5f25c0f997639736
Jaakko Suutarla <jaakko@suutarla.com>
2016-12-23 09:41:47 +0200

    Release 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-kinesis-client "0.1.0"
+(defproject clj-kinesis-client "1.0.0"
   :description "Clojure AWS Kinesis client"
   :url "https://github.com/adtile/clj-kinesis-client"
   :license {:name "Eclipse Public License"
